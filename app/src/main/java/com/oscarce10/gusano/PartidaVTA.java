49e9e1babb6d8777b1fee1b436bfa50a425d4572
Oscar Cely <oscarcej@gmail.com>
2020-07-14 19:17:34 -0500

    Se ajustan imagenes a los cuadros

--- app/src/main/java/com/oscarce10/gusano/PartidaVTA.java
+++ app/src/main/java/com/oscarce10/gusano/PartidaVTA.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.view.Display;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -21,7 +22,8 @@ public class PartidaVTA implements Observer {
     private TextView recordNum;
     private GridLayout grilla;
     private ConstraintLayout main;
-    private TextView tiles[][];
+    private ConstraintLayout tiles[][];
+    private Juego juego;
 
     public PartidaVTA(TextView scoreNum, TextView recordNum, GridLayout grilla) {
         this.scoreNum = scoreNum;
@@ -30,6 +32,7 @@ public class PartidaVTA implements Observer {
     }
 
     public PartidaVTA(Juego juego, int record) {
+        this.juego = juego;
         this.scoreNum = juego.findViewById(R.id.scoreNum);
         this.recordNum = juego.findViewById(R.id.recordNum);
         this.grilla = juego.findViewById(R.id.grilla);
@@ -43,18 +46,18 @@ public class PartidaVTA implements Observer {
 
         this.recordNum.setText("" + record);
 
-        tiles = new TextView[Tablero.ALTO][Tablero.ANCHO];
+        tiles = new ConstraintLayout[Tablero.ALTO][Tablero.ANCHO];
         this.grilla.setRowCount(Tablero.ALTO);
         grilla.setColumnCount(Tablero.ANCHO);
         int c = 1;
         for (int i = 0; i < Tablero.ALTO; i++){
             for (int j = 0; j < Tablero.ANCHO; j++){
-                tiles[i][j] = new TextView(juego);
-                tiles[i][j].setText("" + (c++));
-                tiles[i][j].setTextSize(10);
+                tiles[i][j] = new ConstraintLayout(juego);
                 tiles[i][j].setBackgroundResource(R.drawable.tile);
-                tiles[i][j].setHeight((int) (height * 0.65 / Tablero.ALTO));
-                tiles[i][j].setWidth((int) (width * 0.90 / Tablero.ANCHO));
+                tiles[i][j].setMinHeight((int) (height * 0.65 / Tablero.ALTO));
+                tiles[i][j].setMinWidth((int) (width * 0.90 / Tablero.ANCHO));
+                tiles[i][j].setMaxHeight((int) (height * 0.65 / Tablero.ALTO));
+                tiles[i][j].setMaxWidth((int) (width * 0.90 / Tablero.ANCHO));
                 grilla.addView(tiles[i][j]);
             }
         }
@@ -163,28 +166,42 @@ public class PartidaVTA implements Observer {
         int direccion = Integer.parseInt(args.get(1).toString());
         Coordenada fruta = (Coordenada) args.get(2);
         for (int i = 0; i < gusano.getGusano().size(); i++){
+            ImageView gus = new ImageView(this.juego);
             if(i == 0){
                 switch (direccion){
                     case Gusano.ABAJO:
-                        tiles[gusano.getGusano().get(0).getFila()][gusano.getGusano().get(0).getColumna()].setBackgroundResource(R.drawable.ic_headgamedown);
+                        gus.setBackgroundResource(R.drawable.ic_headgamedown);
                         break;
 
                     case Gusano.ARRIBA:
-                        tiles[gusano.getGusano().get(0).getFila()][gusano.getGusano().get(0).getColumna()].setBackgroundResource(R.drawable.ic_headgameup);
+                        gus.setBackgroundResource(R.drawable.ic_headgameup);
                         break;
 
                     case Gusano.DERECHA:
-                        tiles[gusano.getGusano().get(0).getFila()][gusano.getGusano().get(0).getColumna()].setBackgroundResource(R.drawable.ic_headgameright);
+                        gus.setBackgroundResource(R.drawable.ic_headgameright);
                         break;
 
                     case Gusano.IZQUIERDA:
-                        tiles[gusano.getGusano().get(0).getFila()][gusano.getGusano().get(0).getColumna()].setBackgroundResource(R.drawable.ic_headgameleft);
+                        gus.setBackgroundResource(R.drawable.ic_headgameleft);
                         break;
                 }
+
             }else {
-                tiles[gusano.getGusano().get(i).getFila()][gusano.getGusano().get(i).getColumna()].setBackgroundResource(R.drawable.ic_bodygame);
+                gus.setBackgroundResource(R.drawable.ic_bodygame);
             }
+            gus.setMaxHeight(tiles[0][0].getMaxHeight());
+            gus.setMinimumHeight(tiles[0][0].getMaxHeight());
+            gus.setMinimumWidth(tiles[0][0].getMinWidth());
+            gus.setMaxWidth(tiles[0][0].getMaxWidth());
+            tiles[gusano.getGusano().get(i).getFila()][gusano.getGusano().get(i).getColumna()].addView(gus);
         }
-        tiles[fruta.getFila()][fruta.getColumna()].setBackgroundResource(R.drawable.ic_apple);
+        ImageView apple = new ImageView(this.juego);
+        apple.setBackgroundResource(R.drawable.ic_apple);
+        apple.setMaxHeight(tiles[0][0].getMaxHeight());
+        apple.setMinimumHeight(tiles[0][0].getMaxHeight());
+        apple.setMinimumWidth(tiles[0][0].getMinWidth());
+        apple.setMaxWidth(tiles[0][0].getMaxWidth());
+        tiles[fruta.getFila()][fruta.getColumna()].addView(apple);
+
     }
 }
