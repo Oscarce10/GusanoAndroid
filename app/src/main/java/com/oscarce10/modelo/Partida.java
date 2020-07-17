36d2de44b6e0a1da1d124753ad51f6204c70fc5a
Oscar Cely <oscarcej@gmail.com>
2020-07-16 22:26:01 -0500

    Antes de agregar fruta

--- app/src/main/java/com/oscarce10/modelo/Partida.java
+++ app/src/main/java/com/oscarce10/modelo/Partida.java
@@ -1,5 +1,8 @@
 package com.oscarce10.modelo;
 
+import android.os.Looper;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Timer;
@@ -71,6 +74,7 @@ public class Partida extends Observable {
         ArrayList <Object> args = new ArrayList<>();
         // Se toma la cola del gusano
         Coordenada cola = this.gusano.getGusano().get(this.gusano.getGusano().size() - 1);
+        // Se toma la cabeza del gusano
         Coordenada cabeza = this.gusano.getGusano().get(0);
 
         // Cada coordenada del cuerpo la hace igual a la anterior tanto en las coordenadas como en el tablero y todo se vuelve cuerpo
@@ -133,96 +137,34 @@ public class Partida extends Observable {
             args.add(PERDER);
             this.setChanged();
             this.notifyObservers(args);
+            return false;
         }
 
-        // Si en el tablero donde se para la cabeza esta la fruta
-        if (this.obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] == Tablero.FRUTA){
-            // Se añade la cola guardada previamente al gusano y no se cambia en el tablero la coordenada ya que sigue siendo CUERPO
-            this.gusano.getGusano().add(cola);
-            // Se marca directriz de AGREGAR
-            args.add(SUMAR);
-            // Se agrega la nueva fruta para mostrar
-            Coordenada nuevaFruta = this.obT.agregarFruta();
-            this.obT.getTablero()[nuevaFruta.getFila()][nuevaFruta.getColumna()] = Tablero.FRUTA;
-            args.add(nuevaFruta);
-        }
-        // Si en el tablero donde se para la cabeza NO esta la fruta
-        else {
-            // Se borra la acual cola del tablero
+        if (this.obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] == 0){
+            // Se borra la actual cola del tablero
             this.obT.getTablero()[cola.getFila()][cola.getColumna()] = 0;
             // 0. Se marca directriz de REMOVER
             args.add(REMOVER);
-            // 1. Se envia la antigua cabeza a ser cambiada por cuerpo
-            args.add(cabeza);
+        }
+        // Si aggara la fruta
+        else {
+            // 0. Se marca directriz de REMOVER
+            args.add(SUMAR);
         }
 
-        // En el tablero se marca la cabeza
+        // En tablero se hace la nueva cabeza
         this.obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] = Tablero.CABEZA;
-
-        // 2. Se envia solo la cola para ser removida o sumada
+        // 2. Se envia la cola antigua para ser removida o no
         args.add(cola);
-
         // 3. Se envia solo la cabeza para ser agregada a la vista
         args.add(this.gusano.getGusano().get(0));
-
-        // 4. Se envia la direccion del gusano
+        // 4. Se envia solo la cola nueva para ser dibujada
+        args.add(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1));
+        // 5. Se envia la direccion del gusano
         args.add(this.gusano.getDireccion());
         this.setChanged();
         this.notifyObservers(args);
         return true;
-
-//        // SI LA NUEVA CABEZA NO SE PARA SOBRE LA FRUTA
-//        if (this.obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] != Tablero.FRUTA){
-//            // Tablero[][] cola = 0
-//            this.obT.getTablero()[this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila()]
-//                    [this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna()] = 0;
-//            // Se crea la coordenada de la cola a remover de la vista
-//            cola = new Coordenada(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila(),
-//                    this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna());
-//            // Cada coordenada del cuerpo la hace igual a la anterior
-//            for (int i = this.gusano.getGusano().size() - 1; i >= 1 ; i++){
-//                this.gusano.getGusano().get(i).setFila(this.gusano.getGusano().get(i - 1).getFila());
-//                this.gusano.getGusano().get(i).setColumna(this.gusano.getGusano().get(i - 1).getColumna());
-//            }
-//        }
-
-
-
-//        // Si la cabeza agarra una fruta
-//        if (obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] == Tablero.FRUTA){
-//            // 1. Se envia el argumento de sumar a la vista
-//            args.add(SUMAR);
-//            Coordenada nuevo = null;
-//            switch (this.gusano.getDireccion()){
-//                case Gusano.DERECHA:
-//                    nuevo = new Coordenada(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila(), this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna() - 1);
-//                    break;
-//                case Gusano.IZQUIERDA:
-//                    nuevo = new Coordenada(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila(), this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna() + 1);
-//                    break;
-//                case Gusano.ABAJO:
-//                    nuevo = new Coordenada(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila() - 1, this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna());
-//                    break;
-//                case Gusano.ARRIBA:
-//                    nuevo = new Coordenada(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getFila() + 1, this.gusano.getGusano().get(this.gusano.getGusano().size() - 1).getColumna());
-//                    break;
-//            }
-//            this.gusano.getGusano().add(nuevo);
-//            this.obT.getTablero()[nuevo.getFila()][nuevo.getColumna()] = Tablero.CUERPO;
-//            this.obT.agregarFruta();
-//        } else{
-//            // 1. SINO, Se envia el argumento de remover a la vista
-//            args.add(Partida.REMOVER);
-//        }
-//        // En tablero se hace la nueva cabeza
-//        this.obT.getTablero()[this.gusano.getGusano().get(0).getFila()][this.gusano.getGusano().get(0).getColumna()] = Tablero.CABEZA;
-//        // 2. Se envia solo la cola para ser removida o sumada
-//        args.add(cola);
-//        // 3. Se envia solo la cabeza para ser agregada a la vista
-//        args.add(this.gusano.getGusano().get(0));
-//        // 4. Se envia solo la cola nueva para ser dibujada
-//        args.add(this.gusano.getGusano().get(this.gusano.getGusano().size() - 1));
-
     }
 
     public void cambiarDireccion(int direccion) {
